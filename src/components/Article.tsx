dfa3e539f61ed24a1373c773a613b7bfefdb3da4
shamo-9118 <won9118391183@gmail.com>
2024-04-13 23:33:38 +0900

    タスク詳細ページ実装

---
+++ src/components/Article.tsx
@@ -0,0 +1,60 @@
+import { useState, useEffect} from 'react'
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+type Props = {
+  taskId: string | string[] | undefined;
+}
+
+export const Article = (props: Props) => {
+  const [todoData, setTodoData] = useState<Todo>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  useEffect(()=>{
+    const fetchTaskData = async () => {
+      try{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${props.taskId}`)
+        if (!response.ok) {
+          throw new Error(`ネットワークエラー:  ${response.status}`)
+        }
+        const data: Todo = await response.json()
+        console.log(data)
+        setTodoData(data)
+      } catch (error) {
+        console.error('データを取得できませんでした:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchTaskData()
+  },[])
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>ユーザーID</td>
+          <td>{todoData?.userId}</td>
+        </tr>
+        <tr>
+          <td>タスクID</td>
+          <td>{todoData?.id}</td>
+        </tr>
+        <tr>
+          <td>タスクタイトル</td>
+          <td>{todoData?.title}</td>
+        </tr>
+        <tr>
+          <td>完了ステータス</td>
+          <td>
+            <input type="checkbox" checked={todoData?.completed} />
+          </td>
+        </tr>
+      </table>
+    </div>
+  )
+}
---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,14 @@
+import { Layout } from '@/components/Layout';
+import { Article } from '@/components/Article';
+import { useRouter } from 'next/router'
+
+export default function TodoPage() {
+  const router = useRouter()
+  const taskId = router.query.id
+
+  return (
+    <Layout heading={'Todo詳細'}>
+      <Article taskId={taskId}/>
+    </Layout>
+  )
+}
