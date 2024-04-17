5d3a9dea9237410331258fecd5f6a21da7f29be6
shamo-9118 <won9118391183@gmail.com>
2024-04-18 07:50:45 +0900

    footerが下くるようにスタイル変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,11 +11,13 @@ type Props = {
 
 export const Layout: FunctionComponent<Props> = (props) => {
   return (
-    <div className='flex flex-col'>
-      <Header />
-      <Main heading={props.heading}>
-        {props.children}
-      </Main>
+    <div className='flex flex-col justify-between h-full min-h-[100vh]'>
+      <div className='flex-1'>
+        <Header />
+        <Main heading={props.heading}>
+          {props.children}
+        </Main>
+      </div>
       <Footer />
     </div>
   )
