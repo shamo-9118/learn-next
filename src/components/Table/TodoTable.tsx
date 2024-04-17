bd485947fbe36fc520beb3063dae2bbbabce35e3
shamo-9118 <won9118391183@gmail.com>
2024-04-17 23:43:49 +0900

    useridでフィルターをかける処理を実装

--- src/components/Table/TodoTable.tsx
+++ src/components/Table/TodoTable.tsx
@@ -10,6 +10,7 @@ type Todo = {
 
 export const TodoTable = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [isSelectedUserId, setIsSelectedUserId] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -30,38 +31,59 @@ export const TodoTable = () => {
 
     fetchData();
   }, []);
+
   const todoDataKeyList = todos.length > 0 ? Object.keys(todos[0]) : [];
+  const todoUserIdList = Array.from(new Set(todos.map((todo)=> todo.userId)))
+  const getSelectedUserId = (todoUserId: number) => {
+    if (todoUserId === isSelectedUserId) {
+      setIsSelectedUserId(0)
+    }
+    setIsSelectedUserId(todoUserId)
+  }
+
   return (
-    <table className="border-2 w-full">
-      <thead className="border-2 text-center">
-        <tr className="bg-[#bfdcff] font-medium">
-          { 
-            todoDataKeyList.map((todoDataKey) => {
-              return <td className="border-2 text-center p-3" key={todoDataKey}>{todoDataKey}</td>
-            })
-          }
-        </tr>
-      </thead>
-      <tbody className="border-2">
-          {
-            todos.map((todo) => {
-              return (
-                <tr className="border-2 p-3" key={todo.id}>
-                  <td className="border-2 p-3 text-center">{todo.userId}</td>
-                  <td className="border-2 p-3 text-center">{todo.id}</td>
-                  <td className="border-2 p-3">
-                    <Link href={`/todos/${encodeURIComponent(todo.id)}`}>
-                      {todo.title}
-                    </Link>
-                  </td>
-                  <td className="border-2 p-3 text-center">
-                    <input type="checkbox" defaultChecked={todo.completed} />
-                  </td>
-                </tr>
-              )
-            })
-          }
-      </tbody>
-    </table>
+    <>
+      <div className='flex gap-4 mb-4'>
+        {
+          todoUserIdList.map((todoUserId)=>{
+            return <button onClick={() => getSelectedUserId(todoUserId)} className='w-full bg-sky-600 text-white rounded-md' key={todoUserId}>{todoUserId}</button>;
+          })
+        }
+      </div>
+      <table className='border-2 w-full'>
+        <thead className='border-2 text-center'>
+          <tr className='bg-[#bfdcff] font-medium'>
+            { 
+              todoDataKeyList.map((todoDataKey) => {
+                return <td className='border-2 text-center p-3' key={todoDataKey}>{todoDataKey}</td>
+              })
+            }
+          </tr>
+        </thead>
+        <tbody className='border-2'>
+            {
+              todos.map((todo) => {
+                if(isSelectedUserId !== 0) {
+                  if (todo.userId !== isSelectedUserId) return
+                }
+                return (
+                  <tr className='border-2 p-3' key={todo.id}>
+                    <td className='border-2 p-3 text-center'>{todo.userId}</td>
+                    <td className='border-2 p-3 text-center'>{todo.id}</td>
+                    <td className='border-2 p-3'>
+                      <Link href={`/todos/${encodeURIComponent(todo.id)}`}>
+                        {todo.title}
+                      </Link>
+                    </td>
+                    <td className='border-2 p-3 text-center'>
+                      <input type="checkbox" defaultChecked={todo.completed} />
+                    </td>
+                  </tr>
+                )
+              })
+            }
+        </tbody>
+      </table>
+    </>
   )
 }
