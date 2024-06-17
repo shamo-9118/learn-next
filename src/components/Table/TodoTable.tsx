e41d63b34fed14967a87e5234b97b95c9ce4a3b6
shamo-9118 <won9118391183@gmail.com>
2024-06-18 05:52:52 +0900

    不要な型を削除

--- src/components/Table/TodoTable.tsx
+++ src/components/Table/TodoTable.tsx
@@ -12,14 +12,9 @@ export const TodoTable = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [isSelectedUserId, setIsSelectedUserId] = useState(0);
   const [selectedStatus, setSelectedStatus] = useState('notSelected');
-  const [searchConditionCharacter, setSearchConditionCharacter] =
-    useState<string>('');
+  const [searchConditionCharacter, setSearchConditionCharacter] = useState('');
   const [selectedArrangementType, setSelectedArragementType] =
-    useState<string>('ascending_order');
-  const [isLoading, setIsLoading] = useState(true);
-
-  const text =
-    'laboriosam mollitia et enim quasi adipisci quia provident illum';
+    useState('ascending_order');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,8 +29,6 @@ export const TodoTable = () => {
         setTodos(data);
       } catch (error) {
         console.error('データを取得できませんでした:', error);
-      } finally {
-        setIsLoading(false);
       }
     };
 
@@ -85,120 +78,53 @@ export const TodoTable = () => {
   }, [selectedArrangementType]);
 
   return (
-    <>
-      <div className='space-y-3 mb-6 bg-neutral-100 py-4 px-5 rounded-md'>
-        <div>
-          <label className='flex flex-col max-w-[300px]'>
-            <span className='text-sm'>検索ボックス</span>
-            <input
-              className='border-[2px] border-neutral-300 rounded-md px-1'
-              type='text'
-              value={searchConditionCharacter}
-              onChange={handleSearchConditionCharacter}
-            />
-          </label>
-        </div>
-        <div className='flex gap-4 mb-4'>
-          {todoUserIdList.map((todoUserId) => {
+    <table className='border-2 w-full'>
+      <thead className='border-2 text-center'>
+        <tr className='bg-[#bfdcff] font-medium'>
+          {todoDataKeyList.map((todoDataKey) => {
             return (
-              <button
-                onClick={() => getSelectedUserId(todoUserId)}
-                className={`w-full bg-sky-600 text-white rounded-md duration-200 ${
-                  todoUserId === isSelectedUserId
-                    ? ' opacity-25'
-                    : 'opacity-100'
-                }`}
-                key={todoUserId}
-              >
-                {todoUserId}
-              </button>
+              <td className='border-2 text-center p-3' key={todoDataKey}>
+                {todoDataKey}
+              </td>
             );
           })}
-        </div>
-        <div>
-          <label className='flex flex-col max-w-[300px]' htmlFor=''>
-            <span className='text-sm tracking-[-0.08em]'>ステータス</span>
-            <select
-              value={selectedStatus}
-              onChange={handleSelectedStatus}
-              className='border-[2px] border-neutral-300 rounded-md p-1'
-              name=''
-              id=''
-            >
-              <option className='notSelected' value='' selected>
-                選択してください
-              </option>
-              <option value='completed'>完了</option>
-              <option value='incomplete'>未完了</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label className='flex flex-col max-w-[300px]' htmlFor=''>
-            <span className='text-sm tracking-[-0.08em]'>並び替え</span>
-            <select
-              value={selectedArrangementType}
-              onChange={handleSelectedArrangementType}
-              className='border-[2px] border-neutral-300 rounded-md p-1'
-              name=''
-              id=''
-            >
-              <option value='ascending_order' selected>
-                昇順
-              </option>
-              <option value='descending_order'>降順</option>
-            </select>
-          </label>
-        </div>
-      </div>
-      <table className='border-2 w-full'>
-        <thead className='border-2 text-center'>
-          <tr className='bg-[#bfdcff] font-medium'>
-            {todoDataKeyList.map((todoDataKey) => {
-              return (
-                <td className='border-2 text-center p-3' key={todoDataKey}>
-                  {todoDataKey}
-                </td>
-              );
-            })}
-          </tr>
-        </thead>
-        <tbody className='border-2'>
-          {todos.map((todo) => {
-            if (isSelectedUserId !== 0) {
-              if (todo.userId !== isSelectedUserId) return;
-            }
+        </tr>
+      </thead>
+      <tbody className='border-2'>
+        {todos.map((todo) => {
+          if (isSelectedUserId !== 0) {
+            if (todo.userId !== isSelectedUserId) return;
+          }
 
-            if (selectedStatus !== 'notSelected') {
-              if (selectedStatus === 'completed' && !todo.completed) return;
-              if (selectedStatus === 'incomplete' && todo.completed) return;
-            }
+          if (selectedStatus !== 'notSelected') {
+            if (selectedStatus === 'completed' && !todo.completed) return;
+            if (selectedStatus === 'incomplete' && todo.completed) return;
+          }
 
-            if (searchConditionCharacter.length > 0) {
-              const isExistedSearchTargetCharacter =
-                todo.title.indexOf(searchConditionCharacter) === -1
-                  ? false
-                  : true;
-              if (!isExistedSearchTargetCharacter) return;
-            }
+          if (searchConditionCharacter.length > 0) {
+            const isExistedSearchTargetCharacter =
+              todo.title.indexOf(searchConditionCharacter) === -1
+                ? false
+                : true;
+            if (!isExistedSearchTargetCharacter) return;
+          }
 
-            return (
-              <tr className='border-2 p-3' key={todo.id}>
-                <td className='border-2 p-3 text-center'>{todo.userId}</td>
-                <td className='border-2 p-3 text-center'>{todo.id}</td>
-                <td className='border-2 p-3'>
-                  <Link href={`/todos/${encodeURIComponent(todo.id)}`}>
-                    {todo.title}
-                  </Link>
-                </td>
-                <td className='border-2 p-3 text-center'>
-                  <input type='checkbox' defaultChecked={todo.completed} />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+          return (
+            <tr className='border-2 p-3' key={todo.id}>
+              <td className='border-2 p-3 text-center'>{todo.userId}</td>
+              <td className='border-2 p-3 text-center'>{todo.id}</td>
+              <td className='border-2 p-3'>
+                <Link href={`/todos/${encodeURIComponent(todo.id)}`}>
+                  {todo.title}
+                </Link>
+              </td>
+              <td className='border-2 p-3 text-center'>
+                <input type='checkbox' defaultChecked={todo.completed} />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
