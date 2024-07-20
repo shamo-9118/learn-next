bf27af6143bf3854213925a3a296187e8e5bae9b
shamo-9118 <won9118391183@gmail.com>
2024-07-21 08:19:05 +0900

    不要なコード削除

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -4,9 +4,6 @@ import { SupabaseClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// console.log('Supabase URL:', supabaseUrl); // デバッグ用ログ
-// console.log('Supabase Key:', supabaseKey); // デバッグ用ログ
-
 let supabase: SupabaseClient | null = null;
 
 if (supabaseUrl && supabaseKey) {
