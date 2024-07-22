4cad0a2ed0e03218e8786d6697bcfda573394918
shamo-9118 <won9118391183@gmail.com>
2024-07-22 22:05:24 +0900

    コンポーネントから不要な処理削除

--- src/pages/note/index.tsx
+++ src/pages/note/index.tsx
@@ -1,79 +1,21 @@
-import { useEffect, useState } from 'react';
-import supabase from '@/supabase/client';
+import { useState } from 'react';
 import NoteList from '@/components/Note/NoteList';
 import NoteEditor from '@/components/Note/NoteEditor';
+import { useNotes } from '@/hooks/note/useNotes';
 
 import type { Note } from '@/types/note/note';
 
 const Note = () => {
-  const [notes, setNotes] = useState<Note[]>([]);
   const [previewMode, setPreviewMode] = useState(false);
-  const [currentNoteId, setCurrentNoteId] = useState<number | null>(null);
 
-  useEffect(() => {
-    fetchNotes();
-
-    if (!supabase) return;
-    const mySubscription = supabase
-      .channel('notion-demo-app')
-      .on(
-        'postgres_changes',
-        { event: 'UPDATE', schema: 'public', table: 'notion-demo-app' },
-        fetchNotes,
-      )
-      .subscribe();
-
-    return () => {
-      supabase && supabase.removeChannel(mySubscription);
-    };
-  }, []);
-
-  const fetchNotes = async () => {
-    if (!supabase) return;
-    const { data, error } = await supabase
-      .from('notion-demo-app')
-      .select('*')
-      .order('id', { ascending: false });
-    if (!data) return;
-    setNotes(data);
-    if (error) console.error('Error fetching notes', error);
-  };
-
-  const handleNewNote = async () => {
-    if (!supabase) return;
-    const { data, error } = await supabase
-      .from('notion-demo-app')
-      .insert({ title: '新規ノート', content: '' });
-    if (error || data) {
-      console.error(error);
-      return;
-    }
-
-    fetchNotes();
-  };
-
-  const handleContentChange = async (content: string) => {
-    if (!supabase) return;
-    const { error } = await supabase
-      .from('notion-demo-app')
-      .update({ content })
-      .eq('id', notes[0].id);
-    if (error) console.error('Error updating note', error);
-
-    fetchNotes();
-  };
-
-  const handleChangeTitle = async (title: string) => {
-    if (!supabase) return;
-    const { error } = await supabase
-      .from('notion-demo-app')
-      .update({ title })
-      .eq('id', currentNoteId);
-
-    if (error) {
-      console.error('Error updating note', error);
-    }
-  };
+  const {
+    notes,
+    currentNoteId,
+    setCurrentNoteId,
+    handleNewNote,
+    handleContentChange,
+    handleChangeTitle,
+  } = useNotes();
 
   return (
     <div className='flex h-screen'>
