54d94f9aa088fe5bb24b8940225439587fa61f0d
shamo-9118 <won9118391183@gmail.com>
2024-07-22 22:04:50 +0900

    fetch関数作成

--- src/utils/note/fetchNotes.ts
+++ src/utils/note/fetchNotes.ts
@@ -1,11 +1,20 @@
 import supabase from '@/supabase/client';
+import type { Note } from '@/types/note/note';
 
-export const fetchNotes = async () => {
-  if (!supabase) return;
+export const fetchNotes = async (): Promise<Note[]> => {
+  if (!supabase) {
+    return [];
+  }
+
   const { data, error } = await supabase
     .from('notion-demo-app')
     .select('*')
     .order('id', { ascending: false });
-  if (error) console.error('Error fetching notes', error);
-  if (data) return data;
+
+  if (error) {
+    console.error('Error fetching notes', error);
+    return [];
+  }
+
+  return data || [];
 };
