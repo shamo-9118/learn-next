7e078c08574dbfe967da46794ae7a259cb54e39a
shamo-9118 <won9118391183@gmail.com>
2024-07-24 23:21:31 +0900

    元に戻す

--- src/hooks/usefetchData.ts
+++ src/hooks/usefetchData.ts
@@ -11,7 +11,7 @@ export const useFetchData = (path: string) => {
       );
 
       if (!response.ok) {
-        throw new Error(`ネットワークエラ:  ${response.status}`);
+        throw new Error(`ネットワークエラー:  ${response.status}`);
       }
 
       const data: Todo[] = await response.json();
